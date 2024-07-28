68d44a3fdd19456576d5951664a78acbae4025d0
jeet1703 <prabhjeetdec03@gmail.com>
2024-07-29 00:16:22 +0530

    Intial

---
+++ src/Components/ChessBoard.tsx
@@ -0,0 +1,94 @@
+import { Chess, Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+import { MOVE } from "../screens/Game";
+
+export const ChessBoard = ({
+  chess,
+  board,
+  socket,
+  setBoard,
+}: {
+  chess: Chess;
+  setBoard: React.Dispatch<
+    React.SetStateAction<
+      ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color;
+      } | null)[][]
+    >
+  >;
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+  socket: WebSocket;
+}) => {
+  const [from, setFrom] = useState<null | Square>(null);
+
+  return (
+    <div className="text-white-200">
+      {board.map((row, i) => {
+        return (
+          <div key={i} className="flex">
+            {row.map((square, j) => {
+              const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
+                "" +
+                (8 - i)) as Square;
+
+              return (
+                <div
+                  onClick={() => {
+                    if (!from) {
+                      setFrom(squareRepresentation);
+                    } else {
+                      socket.send(
+                        JSON.stringify({
+                          type: MOVE,
+                          payload: {
+                            move: {
+                              from,
+                              to: squareRepresentation,
+                            },
+                          },
+                        })
+                      );
+                      console.log("Sent MOVE message", { from, to: squareRepresentation });
+
+                      setFrom(null);
+                      chess.move({
+                        from,
+                        to: squareRepresentation,
+                      });
+                      setBoard(chess.board());
+                      console.log("Move made locally", { from, to: squareRepresentation });
+                    }
+                  }}
+                  key={j}
+                  className={`w-16 h-16 ${
+                    (i + j) % 2 === 0 ? "bg-green-500" : "bg-slate-500"
+                  }`}
+                >
+                  <div className="w-full justify-center flex h-full">
+                    <div className="h-full justify-center flex flex-col">
+                      {square ? (
+                        <img
+                          className="w-14"
+                          src={`/${square.color}${square.type}.png`}
+                          alt={`${square.color === "b" ? "black" : "white"} ${
+                            square.type
+                          }`}
+                        />
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
